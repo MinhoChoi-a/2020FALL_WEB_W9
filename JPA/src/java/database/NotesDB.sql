25fc7bf6346dbdb610e4e0a644aead53b759d64a
minho <minhocalgary@gmail.com>
2020-11-05 11:28:17 -0700

    made persistent.xml & model from db

--- JPA/src/java/database/NotesDB.sql
+++ JPA/src/java/database/NotesDB.sql
@@ -1,16 +1,37 @@
-DROP DATABASE if exists NotesDB;
-CREATE DATABASE NotesDB;
+DROP DATABASE if exists notesdb;
+CREATE DATABASE notesdb;
 
-USE NotesDB;
+USE notesdb;
 
-CREATE TABLE users (
+CREATE TABLE if not exists user (
     username VARCHAR(20) NOT NULL, 
     password VARCHAR(20) NOT NULL, 
     firstname VARCHAR(20), 
     lastname VARCHAR(20), 
     email VARCHAR(40), 
-    CONSTRAINT PK_username PRIMARY KEY (username));
+    
+    CONSTRAINT PK_username PRIMARY KEY (username)
+);
 
-INSERT INTO users(username, password)
+
+CREATE TABLE note (
+
+note_id    int(11) NOT NULL AUTO_INCREMENT,    
+username VARCHAR(20) NOT NULL,
+text    VARCHAR(100) NOT NULL,
+
+PRIMARY KEY (note_id),
+CONSTRAINT FK_id FOREIGN KEY (username)
+REFERENCES user(username)
+
+);
+
+INSERT INTO user(username, password)
 VALUES("admin","password");
+
+
+INSERT INTO note(username, text)
+VALUES("admin","password");
+
+
 COMMIT;
---
+++ JPA/src/java/models/Note.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author minho
+ */
+@Entity
+@Table(name = "note")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Note.findAll", query = "SELECT n FROM Note n")
+    , @NamedQuery(name = "Note.findByNoteId", query = "SELECT n FROM Note n WHERE n.noteId = :noteId")
+    , @NamedQuery(name = "Note.findByText", query = "SELECT n FROM Note n WHERE n.text = :text")})
+public class Note implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "note_id")
+    private Integer noteId;
+    @Basic(optional = false)
+    @Column(name = "text")
+    private String text;
+    @JoinColumn(name = "username", referencedColumnName = "username")
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    private User username;
+
+    public Note() {
+    }
+
+    public Note(Integer noteId) {
+        this.noteId = noteId;
+    }
+
+    public Note(Integer noteId, String text) {
+        this.noteId = noteId;
+        this.text = text;
+    }
+
+    public Integer getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(Integer noteId) {
+        this.noteId = noteId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getUsername() {
+        return username;
+    }
+
+    public void setUsername(User username) {
+        this.username = username;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (noteId != null ? noteId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Note)) {
+            return false;
+        }
+        Note other = (Note) object;
+        if ((this.noteId == null && other.noteId != null) || (this.noteId != null && !this.noteId.equals(other.noteId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "models.Note[ noteId=" + noteId + " ]";
+    }
+    
+}
